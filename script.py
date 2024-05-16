9e5fed1f787d66c7c852e2e13a766cbaf27becf9
Pedro Moreira Lopes <73306402+P3TAR@users.noreply.github.com>
2024-05-16 13:20:29 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import json
 import asyncio
 from telegram import Bot
 import datetime
+import time
 
 def send_request(url, method='GET', headers=None, params=None, data=None):
     try:
@@ -22,30 +23,35 @@ async def main():
     # Define the URL, method, headers, params, and data for your request
     url = 'https://csgoempire.com/api/v2/trading/items'
     method = 'GET'
-    params = {'is_commodity': 'false', 'per_page': '100', 'page': '1', 'price_max_above': '-6', 'sort': 'asc', 'order': 'market_value', 'price_min': '1000'}
+    params = {'is_commodity': 'false', 'per_page': '100', 'page': '1', 'price_max_above': '-8', 'sort': 'asc', 'order': 'market_value', 'price_min': '1000', 'price_max': '3259'}
     headers = {'User-Agent': 'PostmanRuntime/7.39.0', 'Authorization': '72b508a8fe5de57cf78a361a5b0d9dd0'}
-    counter = 1
     # Send the request
-    response_json = send_request(url, method, headers, params)  # Now returns JSON directly
-    print(response_json)
-    if response_json is not None and 'data' in response_json:
+    while True:
+        counter = 1
         try:
-            # Check if the item is not a commodity
-            for item in response_json['data']:
-                
-                if isinstance(item, dict) and not item.get('is_commodity', False):
-                    if item['id'] not in sent_items:
-                    # Send a message via Telegram
-                        bot_token = '7020838298:AAHOGeJxt6NwjaG9h4d8AMu_fcFhHuXwt4o'
-                        chat_id = '6347554334'
-                        now = datetime.datetime.now().replace(microsecond=0)
-                        
-                        message = f"COUNTER: {counter}\nItem: {item['market_name']}\nCurrent Price: {item['market_value']}\nSuggested price: {item['suggested_price']}\ndiscount: {item['above_recommended_price']}.\nDate and Time: {now.strftime('%d/%m/%Y %H:%M:%S')}\n------------------------------------------------------------------------------------"
-                        counter += 1
-                        await send_telegram_message(bot_token, chat_id, message)
-
+            response_json = send_request(url, method, headers, params)
+            if response_json is not None and 'data' in response_json:
+                try:
+                    for item in response_json['data']:
+                        if isinstance(item, dict) and not item.get('is_commodity', False):
+                            if item['id'] not in sent_items:
+                                bot_token = '7020838298:AAHOGeJxt6NwjaG9h4d8AMu_fcFhHuXwt4o'
+                                chat_id = '6347554334'
+                                now = datetime.datetime.now().replace(microsecond=0)
+                                
+                                item_id = item['id']
+                                item_url = f"https://csgoempire.com/item/{item_id}"
+                                message = f"COUNTER: {counter}\nItem: {item['market_name']}\nCurrent Price: {item['market_value']}\nSuggested price: {item['suggested_price']}\ndiscount: {item['above_recommended_price']}.\nDate and Time: {now.strftime('%d/%m/%Y %H:%M:%S')}\nItem URL: {item_url}\n------------------------------------------------------------------------------------"
+                                counter += 1
+                                await send_telegram_message(bot_token, chat_id, message)
+                                
+                                sent_items.add(item['id'])
+                except json.JSONDecodeError as e:
+                    print(f"Failed to parse response as JSON: {e}")
+            time.sleep(10)
         except json.JSONDecodeError as e:
             print(f"Failed to parse response as JSON: {e}")
-
+        
 if __name__ == '__main__':
     asyncio.run(main())
+    
